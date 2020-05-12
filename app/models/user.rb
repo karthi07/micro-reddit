0dac3dca81a6ab6f9593d133a1e918c934bf5de7
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-05-12 18:39:45 +0530

    add post model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username, presence: true, uniqueness: true, length: { minimum: 2, maximum: 30 }
-  validates :email, presence: true, uniqueness: { case_sensitive: false}, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness:
+                  { case_sensitive: false}, format:
+                  { with: VALID_EMAIL_REGEX }
+  has_many :posts
 end
